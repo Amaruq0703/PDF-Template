766e7a34897bf1e53787301619fd27af4c2ec29a
Aditya Khera <aditya.khera7@gmail.com>
2023-09-15 16:49:31 +0100

    Added comments

--- main.py
+++ main.py
@@ -16,27 +16,29 @@ for index, row in df.iterrows():
     pdf.cell(w=0, h=12, txt=row['Topic'], align ='L', ln=1)
     pdf.line(10, 20 , 200, 20)
 
-#Main page lines
+    #Main page lines
 
     for i in range(30, 270, 10):
         pdf.line(10, i, 200, i)
 
-# Main page footer
+    # Main page footer
 
     pdf.ln(260)
     pdf.set_font(family='Times', size=10, style="I")
     pdf.set_text_color(0, 0, 0)
     pdf.cell(w=0, h=10, txt=row['Topic'], align='R')
 
+    #Supplement pages
+
     for i in range(row['Pages']-1):
         pdf.add_page()
 
-#Supplement page lines
+        #Supplement page lines
 
         for i in range(30, 270, 10):
             pdf.line(10, i, 200, i)
 
-# Supplement footer
+        # Supplement footer
 
         pdf.ln(273)
         pdf.set_font(family='Times', size=10, style="I")
